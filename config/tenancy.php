a70d11d173950a6a3d883261726abb5975987128
Muhammad Ayaz <fazlani362@gmail.com>
2026-05-03 15:57:32 +0500

    domain added in tenancy config

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,9 +17,9 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'localhost:8000',
-        'localhost',
-        '127.0.0.1',
+        // 'localhost:8000',
+        // 'localhost',
+        // '127.0.0.1',
         'posforshops.com',
 
     ],
