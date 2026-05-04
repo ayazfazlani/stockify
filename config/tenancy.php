a408fa316d56fc2644d2216ffb67057d4885e7bb
Muhammad Ayaz <fazlani362@gmail.com>
2026-05-04 11:58:41 +0500

    config updated

--- config/tenancy.php
+++ config/tenancy.php
@@ -25,11 +25,10 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        // 'localhost:8000',
-        // 'localhost',
-        // '127.0.0.1',
         'posforshops.com',
-
+        'localhost:8000',
+        'localhost',
+        '127.0.0.1',
     ],
 
     /**
